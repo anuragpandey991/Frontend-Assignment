6c5ed99d0e8c2329baf446b1108379f16bb901e9
Anurag Pandey <96683622+anuragpandey991@users.noreply.github.com>
2024-04-26 02:28:56 +0530

    Update Navbar.jsx

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -31,11 +31,11 @@ function Navbar({onDurationChange, OnClick}) {
           <ul className={`${styles.navMenu} ${isActive ? styles.active : ''}`}>
             <li className={`${hover1 ? styles.hover : styles.none}`} onMouseOver={() => { setHover1(true) }} onMouseLeave={() => { setHover1(false) }}>
               <img src='./pictures/logo.png' className={`${styles.logo}`} />
-              <Link to='https://frontend-assignment-5kb8.vercel.app/metrics' className={`${styles.navLink}`}>Metrics</Link>
+              <Link to='/metrics' className={`${styles.navLink}`}>Metrics</Link>
             </li>
             <li className={`${hover2 ? styles.hover : styles.none}`} onMouseOver={() => { setHover2(true) }} onMouseLeave={() => { setHover2(false) }}>
               <img src='./pictures/logo.png' className={`${styles.logo}`} />
-              <Link to='https://frontend-assignment-5kb8.vercel.app/logs' className={`${styles.navLink}`}>Logs</Link>
+              <Link to='/logs' className={`${styles.navLink}`}>Logs</Link>
             </li>
           </ul>
           <div className={`${styles.dropdown}`}><Dropdown onDurationChange={handleDurationChange} /></div>
