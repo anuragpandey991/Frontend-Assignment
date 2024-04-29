8c4c7ea496bd2e83ca45e1551e2a02b778bb12b1
Anurag Pandey <96683622+anuragpandey991@users.noreply.github.com>
2024-04-30 03:13:18 +0530

    Update Dropdown.jsx

--- my-app/src/components/Dropdown.jsx
+++ my-app/src/components/Dropdown.jsx
@@ -12,11 +12,8 @@ function Dropdown({ onDurationChange }) {
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <select value={selectedDuration} onChange={(e) => handleDurationChange(e.target.value)}>
         <option value="5m">Last 5 minutes</option>
-        <option value="15m">Last 15 minutes</option>
-        <option value="30m">Last 30 minutes</option>
         <option value="1h">Last 1 hour</option>
-        <option value="3h">Last 3 hours</option>
-        <option value="6h">Last 6 hours</option>
+        <option value="1d">Last 1 day</option>
       </select>
     </div>
   );
